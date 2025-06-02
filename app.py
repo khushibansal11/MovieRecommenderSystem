b21994f7a8b5e108fc7e14c65af02d8dcbc98c1e
Khushi Bansal <131777777+khushibansal11@users.noreply.github.com>
2025-06-02 20:20:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,43 @@ import requests
 import pandas as pd
 
 st.title("Movie Recommender System")
-movies_dataset=pickle.load(open('movies_dataset.pkl','rb'))
+movies_dataset=pickle.load(open('movie_list.pkl','rb'))
 similarity=pickle.load(open('similarity.pkl','rb'))
 
 movies_list=movies_dataset['title'].unique()
 movie_selected=st.selectbox("Select movie name",movies_list)
 
 def poster(id):
-    url=f'https://api.themoviedb.org/3/movie/{id}?api_key=8361095de50c93214b053b4d4e8fed5f&language=en-US'
-    response=requests.get(url)
-    data=response.json()
-    return 'https://image.tmdb.org/t/p/original'+data.get('poster_path')
+    print(id)
+    url = f'https://api.themoviedb.org/3/movie/{id}?api_key=8361095de50c93214b053b4d4e8fed5f&language=en-US'
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        print(f"Error fetching poster for ID {id}: {response.status_code}")
+        return 'https://via.placeholder.com/300x450?text=No+Image'
+
+    data = response.json()
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        return 'https://image.tmdb.org/t/p/original' + poster_path
+    else:
+        print(f"No poster path for movie ID {id}")
+        return 'https://via.placeholder.com/300x450?text=No+Image'
+
+
 def recommend(movie):
-    recommended_movies=[]
-    movie_list=[]
-    movie_index=movies_dataset[movies_dataset['title']==movie].index[0]
-    recommended_index=sorted(enumerate(similarity[movie_index]), key=lambda x:x[1],reverse=True)[1:6]
+    recommended_titles = []
+    recommended_ids = []
+    movie_index = movies_dataset[movies_dataset['title'] == movie].index[0]
+    recommended_index = sorted(enumerate(similarity[movie_index]), key=lambda x: x[1], reverse=True)[1:6]
+
     for i in recommended_index:
-        recommended_movies.append(movies_dataset.iloc[i[0],0])
-        movie_list.append(movies_dataset.iloc[i[0],1])
-    return movie_list,recommended_movies
+        recommended_titles.append(movies_dataset.iloc[i[0]].title)
+        recommended_ids.append(movies_dataset.iloc[i[0]].movie_id)  
+
+    return recommended_ids, recommended_titles
+
 
 movie_list,result=recommend(movie_selected)
 col1,col2,col3,col4,col5=st.columns(5)
